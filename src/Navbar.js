193328303714637b4b487d437d4338d39d54be88
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-12 23:55:28 +0530

    logo updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
-import logo from "/logo.svg";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
